c87073e404630bef0cceca2f35b61bad018e18a6
mai <74494945+chan-mai@users.noreply.github.com>
2024-11-05 01:11:19 +0900

    fix: 時限ノートの初期値が正しく設定されない問題を修正

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -476,7 +476,7 @@ function toggleScheduledNoteDelete() {
 	} else {
 		scheduledNoteDelete.value = {
 			deleteAt: null,
-			deleteAfter: null,
+			deleteAfter: defaultStore.state.defaultScheduledNoteDeleteTime,
 			isValid: true,
 		};
 	}
